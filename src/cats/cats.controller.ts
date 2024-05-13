8ae228086be1f3e35b6c8d3371f911f34860eeef
nam1554 <gisubsub@gmail.com>
2024-05-13 16:36:37 +0900

    Route wildcards

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -11,4 +11,14 @@ export class CatsController {
   findAll(@Req() request: Request): string {
     return 'This action returns all cats';
   }
+
+  @Get('ab*cd')
+  findAllWildcard() {
+    //The 'ab*cd' route path will match abcd, ab_cd, abecd, and so on.
+    //The characters ?, +, *, and () may be used in a route path, and are subsets of their regular expression counterparts.
+    //The hyphen ( -) and the dot (.) are interpreted literally by string-based paths.
+    //WARNING
+    //A wildcard in the middle of the route is only supported by express.
+    return 'This route uses a wildcard';
+  }
 }
