b80a9d0ba04e89459f2df08dd246c1e9350032e9
nam1554 <gisubsub@gmail.com>
2024-05-14 11:19:42 +0900

    Full resource sample

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,41 +1,70 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
-  Header,
-  HttpCode,
+  // Header,
+  // HttpCode,
   Param,
   Post,
+  Put,
   Query,
-  Redirect,
-  Req,
+  // Redirect,
+  // Req,
 } from '@nestjs/common';
-import { Observable, of } from 'rxjs';
+// import { Observable, of } from 'rxjs';
 import { CreateCatDto } from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
 
 @Controller('cats')
 export class CatsController {
   @Post()
-  //We can easily change this behavior by adding the @HttpCode(...) decorator at a handler level.
-  // @HttpCode(204)
-  // @Header('Cache-Control', 'none')
-  async create(@Body() createCatDto: CreateCatDto) {
+  create(@Body() createCatDto: CreateCatDto) {
     console.log(createCatDto);
     return 'This action adds a new cat';
   }
 
   @Get()
-  // @Redirect() takes two arguments, url and statusCode, both are optional. The default value of statusCode is 302 (Found) if omitted.
-  // Returned values will override any arguments passed to the @Redirect() decorator.
-  // @Redirect('https://nestjs.com', 301)
-  // getDocs(@Query('version') version: string) {
-  //   if (version && version === '5') {
-  //     return { url: 'https://docs.nestjs.com/v5/' };
-  //   }
-  // }
-  findAll(@Req() request: Request): string {
-    return 'This action returns all cats';
+  findAll(@Query() query: any) {
+    return `This action returns all cats (limit: ${query.limit} items)`;
   }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} cat`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+    console.log(updateCatDto);
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} cat`;
+  }
+  // @Post()
+  // //We can easily change this behavior by adding the @HttpCode(...) decorator at a handler level.
+  // // @HttpCode(204)
+  // // @Header('Cache-Control', 'none')
+  // async create(@Body() createCatDto: CreateCatDto) {
+  //   console.log(createCatDto);
+  //   return 'This action adds a new cat';
+  // }
+
+  // @Get()
+  // // @Redirect() takes two arguments, url and statusCode, both are optional. The default value of statusCode is 302 (Found) if omitted.
+  // // Returned values will override any arguments passed to the @Redirect() decorator.
+  // // @Redirect('https://nestjs.com', 301)
+  // // getDocs(@Query('version') version: string) {
+  // //   if (version && version === '5') {
+  // //     return { url: 'https://docs.nestjs.com/v5/' };
+  // //   }
+  // // }
+  // findAll(@Req() request: Request): string {
+  //   return 'This action returns all cats';
+  // }
   // @Get()
   // async findAll(): Promise<any[]> {
   //   return [];
@@ -45,24 +74,24 @@ export class CatsController {
   //   return of([]);
   // }
 
-  @Get('ab*cd')
-  findAllWildcard() {
-    //The 'ab*cd' route path will match abcd, ab_cd, abecd, and so on.
-    //The characters ?, +, *, and () may be used in a route path, and are subsets of their regular expression counterparts.
-    //The hyphen ( -) and the dot (.) are interpreted literally by string-based paths.
-    //WARNING
-    //A wildcard in the middle of the route is only supported by express.
-    return 'This route uses a wildcard';
-  }
+  // @Get('ab*cd')
+  // findAllWildcard() {
+  //   //The 'ab*cd' route path will match abcd, ab_cd, abecd, and so on.
+  //   //The characters ?, +, *, and () may be used in a route path, and are subsets of their regular expression counterparts.
+  //   //The hyphen ( -) and the dot (.) are interpreted literally by string-based paths.
+  //   //WARNING
+  //   //A wildcard in the middle of the route is only supported by express.
+  //   return 'This route uses a wildcard';
+  // }
 
   // @Get(':id')
   // findOne(@Param() params: any): string {
   //   console.log(params.id);
   //   return `This action returns a #${params.id} cat`;
   // }
-  @Get(':id')
-  findOne(@Param('id') id: string): string {
-    console.log(id);
-    return `This action returns a #${id} cat`;
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string): string {
+  //   console.log(id);
+  //   return `This action returns a #${id} cat`;
+  // }
 }
